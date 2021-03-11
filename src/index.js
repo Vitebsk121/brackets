9c06688e568bf7f5bb09cd4d18c95213c0ef6ddd
Vitebsk121 <Alexandr.demchenko@list.ru>
2021-03-11 16:18:31 +0300

    feal: add check func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrCheck = [];
+  const config = bracketsConfig.flat();
+  const openBrackets = config.filter((item, index) => (index % 2 == 0) && (item !== '|') && (item !== '7') && (item !== '8'));
+  const closedBrackets = config.filter((item, index) => (index % 2 !== 0) && (item !== '|') && (item !== '7') && (item !== '8'));
+  const specialBrackets = config.filter((item) => (item === '|') || (item === '7') || (item === '8'));
+  const configCheck = bracketsConfig.map((item) => item.join(''));
+  const arrOfStr = str.split('');
+  arrOfStr.forEach((item) => {
+    if(openBrackets.includes(item)) { 
+      return arrCheck.push(item);
+  } else if (closedBrackets.includes(item)) {
+       if (configCheck.includes((arrCheck[arrCheck.length-1] + item))) {
+         return arrCheck.pop();
+      } else {
+         return arrCheck.push(item);
+      }
+  } else if (specialBrackets.includes(item)) {
+      if (configCheck.includes((arrCheck[arrCheck.length-1] + item))) {
+        return arrCheck.pop();
+      } else {
+      return arrCheck.push(item);
+      }
+  }
+  });
+  return arrCheck.length === 0;
 }
